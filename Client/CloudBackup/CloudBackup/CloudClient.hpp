5da10cd1cbc0d03ff3d2dd14a85ea2f9e3639d14
Misaki <1761607418@qq.com>
2019-09-26 22:36:03 +0800

    2019.9.26

--- Client/CloudBackup/CloudBackup/CloudClient.hpp
+++ Client/CloudBackup/CloudBackup/CloudClient.hpp
@@ -5,12 +5,15 @@
 #include <unordered_map>
 #include <string>
 #include <fstream>
+#include <sstream>
 #define CLIENT_BACKUP_DIR "backup"
 #define CLIENT_BACKUP_INFO_FILE "back.list"
+#define RANGE_MAX_SIZE (10 << 20)//分块大小
 namespace bf = boost::filesystem;
 class CloudCLient
 {
 private:
+	//filename etag
 	std::unordered_map<std::string, std::string> _backup_list;//存放备份信息
 	//获取文件备份信息，用于判断哪些文件需要备份哪些无需备份
 	bool GetBackupInfo()
@@ -88,21 +91,84 @@ private:
 			return false;
 		}
 		file.close();
+		return true;
 	}
 	//监听目录获取目录文件信息，并且刷新备份信息
-	bool BackupDirListen()
+	bool BackupDirListen(const std::string& path)
 	{
-
+		bf::directory_iterator item_begin(CLIENT_BACKUP_DIR);
+		bf::directory_iterator item_end;
+		//遍历目录
+		for (; item_begin != item_end; item_begin++)
+		{
+			//如果是目录则递归遍历
+			if (bf::is_directory(item_begin->status))
+			{
+				BackupDirListen(item_begin->path().string());
+				continue;
+			}
+			//判断是否需要备份
+			if (FilesIsNeedBackUp(item_begin->path().string()) == false)
+			{
+				continue;
+			}
+			//判断上传是否成功
+			if (PutFileData(item_begin->path().string()) == false)
+			{
+				continue;
+			}
+			//更新备份信息到backup_list
+			AddBackupInfo(item_begin->path().string());
+		}
+	}
+	//更新备份信息etag到backup_list
+	bool AddBackupInfo(const std::string& file)
+	{
+		//etag = mtime-fsize
+		std::string etag;
+		if (GetFileEtag(file, etag) == false)
+		{
+			return false;
+		}
+		_backup_list[file] = etag;
 	}
 	//备份file文件，上传文件数据
+	//多线程分块上传文件
 	bool PutFileData(const std::string& file)
 	{
+		bf::path path(file);
+		int64_t fsize = bf::file_size(path);
 
 	}
 	//判断文件是否需要备份
-	bool FilesNeedBackUp(const std::string& file)
+	bool FilesIsNeedBackUp(const std::string& file)
 	{
-
+		std::string etag;
+		if (GetFileEtag(file, etag) == false)
+		{
+			return false;
+		}
+		std::unordered_map<std::string, std::string>::const_iterator it = _backup_list.find(file);
+		if (it != _backup_list.end() && it->second == etag)
+		{
+			return false;
+		}
+		return true;
+	}
+	bool GetFileEtag(const std::string file, std::string& etag)
+	{
+		bf::path path(file);
+		if (!bf::exists(path))
+		{
+			std::cerr << "get file " << file << " etag error" << std::endl;
+			return false;
+		}
+		int64_t fsize = bf::file_size(path);
+		int64_t mtime = bf::last_write_time(path);
+		std::stringstream ss;
+		ss << std::hex << mtime << "-" << std::hex << fsize;
+		etag = ss.str();
+		return true;
 	}
 public:
 	bool Start()
